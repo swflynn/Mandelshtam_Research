908ef5df9593d2189524cb0065cd4e9b3333f9e5
Moiseser <moiseser@uci.edu>
2018-07-23 10:24:43 -0700

    Generalized, all that is needed is to delete comments and finish the last part with the varince and stdev

--- data_process/mc_error/p_std_dev.f90
+++ data_process/mc_error/p_std_dev.f90
@@ -25,7 +25,7 @@
 program Standard_Deviation_MC
 implicit none
 double precision, allocatable :: mc_data(:,:), partial_avg(:,:) , variance(:,:)
-double precision ,allocatable:: std_deviation(:,:), avg(:,:) , old_avg(:,:)
+double precision ,allocatable:: std_deviation(:,:), avg(:,:) ,  old_avg(:,:)
 integer :: i, j , k , l ,N, M, partial_avg_size , skip , adjust , freq , count,x
 character(50) :: datafile
 !==============================================================================!
@@ -36,14 +36,13 @@ read (*,*) freq
 read (*,*) N
 read (*,*) M
 read(*,*) datafile
-allocate(mc_data(N,M),avg(N,M))
-allocate(std_deviation(1,M),old_avg(1,M))
+allocate(mc_data(N,M))
+allocate(std_deviation(1,M),old_avg(1,M),avg(1,M))
 write (*,*) 'Sucessfully Read in paramters from input file'
 !==============================================================================!
 !Reading in Data file
 !==============================================================================!
-!This will be turned into a matrix loop eventually for matrix sets of data
-!Problem for another day
+
 open (13,file = datafile)
 do i = 1 , N
 read (13,*) mc_data(i,:)
@@ -51,6 +50,13 @@ write(*,*) mc_data(i,:)
 end do
 close(13)
 !write(*,*) mc_data
+!mc_data(:,1) = (/1.0,-3.0,1.0/)
+!mc_data(:,2) = (/2.0,-8.0,8.0/)
+!mc_data(:,3)= (/-6.0,3.0,-15.0/)
+!mc_data(:,1) = (/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0/)
+!mc_data(:,2) = (/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0/)
+!mc_data(:,3) = (/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0/)
+!write (*,*) mc_data(5,:)
 !==============================================================================!
 !Setting Varaibles for Calc Partial Averages
 !==============================================================================!
@@ -59,35 +65,41 @@ adjust = mod((N-skip),freq)
 partial_avg_size = (N - skip - adjust ) / freq
 !write(*,*) 'partial_avg_size is' , partial_avg_size
 allocate (partial_avg(partial_avg_size,M),variance(partial_avg_size,M))
-avg = 0d0
+avg(:,:) = 0d0
 count = 0d0
 partial_avg = 0D0
 x = 0d0
 do i = (1 + skip + adjust) , N
-  avg(i,:) = avg(i,:) + mc_data(i,:)
+  write (*,*) 'avg is ' , avg
+  write(*,*) 'mc_data is', mc_data(i,:)
+  avg(1,:) = avg(1,:) + mc_data(i,:)
   write (*,*) avg
-  !write(*,*) 'some numbers' , avg
+  write(*,*) 'some numbers' , avg
   count = count + 1
+  write (*,*) count
   if (mod(count , freq) == 0 ) then
-    avg = avg / freq
-  !  write (*,*) 'avg/freq is' , avg
+    avg(1,:) = avg(1,:) / freq
+    write (*,*) 'avg/freq is' , avg
     x = x + 1
-   partial_avg(x,:) =  avg(x,:)
+   partial_avg(x,:) =  avg(1,:)
     avg = 0d0
     count = 0d0
   end if
 end do
-write(*,*) 'Avg Array is' , partial_avg
+write(*,*) 'Avg Array is:'
+do i = 1 , partial_avg_size
+  write (*,*) partial_avg(i,:)
+end do
 
 old_avg = 0d0
 do i = (1 + skip + adjust) , N
   old_avg(1,:) = old_avg(1,:) + mc_data(i,:)
 end do
-old_avg = old_avg / (N - (skip+adjust))
+old_avg(1,:) = old_avg(1,:) / (N - (skip+adjust))
 write(*,*) 'old_avg is :' , old_avg
 do i = 1 , partial_avg_size
   variance(i,:) = (partial_avg(i,:) - old_avg(1,:))**2
-  write(*,*) variance
+  write(*,*) variance(i,:)
 end do
 std_deviation(1,:) = sqrt(sum(variance)/(partial_avg_size-1))
 write(*,*) 'Standard Deviation' , std_deviation
